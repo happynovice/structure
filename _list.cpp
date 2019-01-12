0e996c6bae19eacb8e56fd44061cc4a07f39e811
happynovice <36851051+happynovice@users.noreply.github.com>
2019-01-12 13:57:55 +0800

    Update _list.cpp

--- _list.cpp
+++ _list.cpp
@@ -190,3 +190,43 @@ int List<T>::InsertSort()
 	}
 	return 0;
 }
+template<typename T>
+int List<T>::SelectSort()
+{
+	ListNode<T> * PtrSort = trailer;
+	ListNode<T> * PtrBack = header->succ;
+	ListNode<T> * PtrBuff = PtrBack;
+	ListNode<T> * PtrBack2 = PtrBack;
+	for (int i = _size; i > 0; i--)
+	{
+		int MaxValue = 0;
+		PtrBuff= header->succ;
+		PtrBack = PtrBuff;
+		for (int j=0; j<i ;j++)
+		{
+			if (PtrBuff->data > MaxValue)
+			{
+				MaxValue = PtrBuff->data;
+				PtrBack = PtrBuff;
+			}
+			PtrBuff = PtrBuff->succ;
+		}
+		PtrBack->pred->succ = PtrBack->succ;
+		PtrBack->succ->pred = PtrBack->pred;
+
+		PtrSort->pred->succ = PtrBack;
+		PtrBack->pred=PtrSort->pred;
+		PtrSort->pred = PtrBack;
+		PtrBack->succ = PtrSort;
+
+		PtrSort = trailer;
+		for (int k = 0; k <= _size - i; k++)
+		{
+			PtrSort = PtrSort->pred;
+		}
+		
+
+	}
+
+	return 0;
+}
