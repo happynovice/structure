1e1d118ece5f9c9bcc811ebe178a3bbd77f00e39
happynovice <36851051+happynovice@users.noreply.github.com>
2019-01-14 07:59:24 +0800

    Update _vector.h

--- _vector.h
+++ _vector.h
@@ -18,4 +18,35 @@ namespace ISF {
 
 	};
 }
+
+
+template <typename T> class _vector
+{
+protected:
+	T * data;
+	rank _size;
+	int capacity;
+public:
+	int insert(T e, int position);
+	void merge(rank lo, rank mi, rank hi);
+
+	void mergeSort(rank lo, rank hi);
+
+};
+
+template <typename T> 
+int _vector<T>::insert(T e, int position)
+{
+	if (position > _size)
+	{
+		return -1;
+	}
+	for (; position <= _size; position++)
+	{
+		data[position] = data[position-1];
+	}
+	data[position - 1] = e;
+	_size++;
+	return 0;
+}
 #endif
