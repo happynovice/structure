198063b599b02ebe6389f9bde9cccc85ff70fb66
happynovice <36851051+happynovice@users.noreply.github.com>
2019-01-17 21:37:11 +0800

    Update _stack.h

--- _stack.h
+++ _stack.h
@@ -1,9 +1,27 @@
 #pragma once
-template<typename T> class Stack: Vector<T>
+#include"_vector.h"
+
+template<typename T> class Stack :public Vector<T>
 {
 public:
 	Stack()=default;
-	void push(T const& e) { insert(e,size())};
-	T pop() { return remove[size()-1]}
-	T &top() {return (this*)[size()-1]}
+	void push(T const& e) { Vector<T>::insert(e, Vector<T>::size()); };
+	T pop() { return Vector<T>::remove(Vector<T>::size() - 1); }
+	T &top() { return (*this)[Vector<T>::size() - 1]; }
 };
+void convert(Stack<char> & s,int num,int base)
+{
+	int remainder = num % base;
+
+	
+	const char  data[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+	s.push(data[remainder]);
+	num/=base ;
+	if (num == 0)
+	{
+		return;
+	}
+	convert(s,num,base);
+
+}
+
