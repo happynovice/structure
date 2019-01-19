57506e9e1ccc1ea95e0e4904c5c80c61b01e70e5
happynovice <36851051+happynovice@users.noreply.github.com>
2019-01-19 20:44:05 +0800

    Update stack_source.cpp

--- stack_source.cpp
+++ stack_source.cpp
@@ -1,5 +1,4 @@
 #include"_vector.h"
-
 #include<stdio.h>
 #include<iostream>
 #include<vector>
@@ -10,20 +9,28 @@ using std::cout;
 using std::endl;
 int main()
 {
-
 	Vector<int> StackPra(4,2,0);
-	
 	StackPra.insert(2,0);
 	StackPra.PrintData();
 	Stack<char> S;
-	convert(S,1000,16);
+	loop_convert(S,1000,16);
 	int size=S.size();
 	for (int i = 0; i < size; i++)
 	{
 		printf("S.pop() :%c \n", S.pop());
 	}
-	//lscout << "hello world!" << endl;
+	////////////////////////////////////
+	const char  string_data[20] = "()(){22}()()";
+	int ret=paren(string_data, 0, 15);
+	if (0 == ret){
+		printf(" not ret :%d \n", ret);
+	}
+	else {
+		printf("is ret :%d \n", ret);
+	}
+	int result=ComputeRPN("15+34-+",7);
+	printf("ComputeRPN :%d\n", result);
+	//cout << "hello world!" << endl;
 	while (1);
 	return 0;
 }
-
